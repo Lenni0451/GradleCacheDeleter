3e7bfaf65627455b2de3190f665d16d03051125a
William Gray <ruwagray@gmail.com>
2024-08-13 18:19:47 +0000

    fix build. add workflow

--- src/main/java/net/lenni0451/gradlecachedeleter/Main.java
+++ src/main/java/net/lenni0451/gradlecachedeleter/Main.java
@@ -15,7 +15,7 @@ public class Main {
             GRADLE_USER_HOME = new File(System.getProperty("user.home"), ".gradle");
         }
     }
-    
+
     public static final File GRADLE_CACHES = new File(GRADLE_USER_HOME, "caches");
     public static final File GRADLE_MODULES = new File(GRADLE_CACHES, "modules-2");
 
