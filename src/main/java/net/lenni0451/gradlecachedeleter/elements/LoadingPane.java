1088c5dc4095477f943e53c56d8288935bfe2b3a
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-08-13 21:33:24 +0200

    Updated commons

--- src/main/java/net/lenni0451/gradlecachedeleter/elements/LoadingPane.java
+++ src/main/java/net/lenni0451/gradlecachedeleter/elements/LoadingPane.java
@@ -1,29 +1,19 @@
 package net.lenni0451.gradlecachedeleter.elements;
 
+import net.lenni0451.commons.swing.components.OverlayPanel;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
 
-public class LoadingPane extends JPanel {
-
-    private static final MouseAdapter VOID_MOUSE_ADAPTER = new MouseAdapter() {
-    };
-    private static final KeyAdapter VOID_KEY_ADAPTER = new KeyAdapter() {
-    };
-    private static final FocusAdapter STEALING_FOCUS_ADAPTER = new FocusAdapter() {
-        @Override
-        public void focusLost(FocusEvent e) {
-            e.getComponent().requestFocus();
-        }
-    };
-
+public class LoadingPane extends OverlayPanel {
 
     private final Timer animationTimer;
     private String text = "Loading";
 
     public LoadingPane() {
+        super(new Color(0, 0, 0, 127));
         this.animationTimer = new Timer(1000, this::tick);
-        this.setOpaque(false);
     }
 
     private void tick(final ActionEvent event) {
@@ -37,32 +27,16 @@ public class LoadingPane extends JPanel {
         super.addNotify();
         this.requestFocus();
         this.animationTimer.start();
-
-        this.addMouseListener(VOID_MOUSE_ADAPTER);
-        this.addMouseMotionListener(VOID_MOUSE_ADAPTER);
-        this.addMouseWheelListener(VOID_MOUSE_ADAPTER);
-        this.addKeyListener(VOID_KEY_ADAPTER);
-        this.addFocusListener(STEALING_FOCUS_ADAPTER);
     }
 
     @Override
     public void removeNotify() {
         super.removeNotify();
         this.animationTimer.stop();
-
-        this.removeMouseListener(VOID_MOUSE_ADAPTER);
-        this.removeMouseMotionListener(VOID_MOUSE_ADAPTER);
-        this.removeMouseWheelListener(VOID_MOUSE_ADAPTER);
-        this.removeKeyListener(VOID_KEY_ADAPTER);
-        this.removeFocusListener(STEALING_FOCUS_ADAPTER);
     }
 
     @Override
-    public void paintComponent(final Graphics g) {
-        super.paintComponent(g);
-        g.setColor(new Color(0, 0, 0, 127));
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
-
+    protected void paintOverlay(Graphics g) {
         g.setColor(Color.BLACK);
         g.setFont(g.getFont().deriveFont(20f));
         int width = g.getFontMetrics().stringWidth(this.text);
